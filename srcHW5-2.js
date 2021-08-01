89420de242a891ae6de8ca09c8c34627309c60bb
Liubov <kravtsova5.0@yandex.ru>
2021-08-02 01:44:56 +0500

    error correction

--- srcHW5-2.js
+++ srcHW5-2.js
@@ -1,14 +1,14 @@
 'use strict'
 let exRowNum = 0;
 
-class Product {  
-    constructor (id, qt, price) {
+class Product {
+    constructor(id, qt, price) {
         this.id = id;
         this.qt = qt;
         this.price = price;
-        this.sum = this.price*this.qt;     
+        this.sum = this.price * this.qt;
         exRowNum++;
-    }    
+    }
 }
 
 let basket = [];
@@ -20,50 +20,48 @@ basket[exRowNum] = new Product('banana', 3, 70);
 basket[exRowNum] = new Product('pineapple', 3, 150);
 //exampleES6.es6edit();
 //console.log(basket);
- 
 
+
+const textStr = {
+    textReturn(element) {
+        return `<div><i> <p>Наименование: ${element.id} / Количествоp: ${element.qt} / Цена: ${element.price}/ Стоимость: ${element.sum}</p></i></div>`;
+    },
+}
 
 const cart = {
     basket,
-    textReturn: null,
-    total :null,
-   
+    total: null,
     el: null,
-    init() {  
-        if(this.basket.length){
-             this.basket.forEach((element) => {  
-        cart.textStr(element);
-        this.totalSum(element);            
-        cart.cartRender(); })
-        console.log(this.total);
-        cart.totalStr();
-        this.el.insertAdjacentHTML('beforeend', this.totalStr());
-        }else{
+
+
+    init() {  //Позиционирование на HTML-странице
+        this.el = document.querySelector('.myclass');
+        this.cartRender();
+    },
+
+    wtf(element) {
+        return `<div><i> <p>Наименование: ${element.id} / Количествоp: ${element.qt} / Цена: ${element.price}/ Стоимость: ${element.sum}</p></i></div>`;
+    },
+
+    cartRender() {
+        if (this.basket.length) {
+            this.basket.forEach((element) => {
+                this.el.insertAdjacentHTML('beforeend', `<div><i><p>${textStr.textReturn(element)}</p></i></div>`);
+            })
+            this.el.insertAdjacentHTML('beforeend', `<div><i><p>В корзине: ${this.basket.length} товаров на сумму ${this.totalSum()} рублей</p></i></div>`);
+        } else {
             this.el.insertAdjacentHTML('beforeend', 'Корзина пуста');
-        }   
-       
+        }
     },
-    totalSum(element){        
-        this.total+=element.sum;
+
+    totalSum() {
+        return this.basket.reduce(function (sum, element) {
+            return sum + element.sum;
+        }, 0);
     },
-    totalStr(){
-        return `<div><i>
-        <p>В корзине: ${this.basket.length} товаров на сумму ${this.total} рублей</p>
-         </i></div>`;
-    },
-    textStr(element) {
-        this.textReturn = `<div><i>
-                   <p>Наименование: ${element.id}
-                    / Количествоp: ${element.qt}
-                    / Цена: ${element.price}
-                    / Стоимость: ${element.sum}</p>
-                    </i></div>`;
-    },
-    cartRender(){  //Позиционирование на HTML-странице
-    this.el = document.querySelector('.myclass');    
-    this.el.insertAdjacentHTML('beforeend', this.textReturn);
-    },
-}
+};
+
+
 cart.init();
 
 
